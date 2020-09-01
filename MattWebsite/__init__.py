ee56018e0070e30d4b7e3392b72c20f10bcc6667
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-08-31 20:41:33 -0500

    Created Repo

---
+++ MattWebsite/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+#CHANGE THIS
+app.config['SECRET_KEY'] = 'ad15a7e74cae2a04a436d996579c50f9'
+######
+
+
+#Data Base
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app) 
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from MattWebsite import routes
